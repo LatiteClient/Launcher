37d47cba110d6876f894b58efd4174694428a4db
Imrglop <69129770+Imrglop@users.noreply.github.com>
2024-04-15 11:55:38 -0400

    prevent select main menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
+// currently not functional, use index.html
+
+/*
+
 function closeWindowAnimation() {
 	var launcher = document.querySelector(".launcher");
 	launcher.classList.toggle("launcherCloseAnimation");
@@ -13,22 +17,5 @@ function closeWindow() {
 	window.close();
 }
 
-const optionMenu = document.querySelector(".select-menu"),
-	selectBtn = optionMenu.querySelector(".select-btn"),
-	options = optionMenu.querySelectorAll(".option"),
-	sBtn_text = optionMenu.querySelector(".sBtn-text");
-
-selectBtn.addEventListener("click", () =>
-	optionMenu.classList.toggle("active")
-);
-
-/*options.forEach((option) => {
-  option.addEventListener("click", () => {
-	let selectedOption = option.querySelector(".option-text").innerText;
-	sBtn_text.innerText = selectedOption;
-
-	optionMenu.classList.remove("active");
-  });
-});*/
-
 document.addEventListener("contextmenu", (event) => event.preventDefault());
+*/
