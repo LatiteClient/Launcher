62ee778c5cee07d32270a2ed75926381baeb4219
Plextora <71889427+Plextora@users.noreply.github.com>
2026-04-02 23:01:33 -0400

    Added close after inject functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,11 +15,25 @@ use launch_request::InjectRequest;
 use tauri::State;
 
 #[tauri::command]
-async fn inject(request: InjectRequest, state: State<'_, AppState>) -> Result<(), String> {
+async fn inject(
+    request: InjectRequest,
+    state: State<'_, AppState>,
+    app_handle: tauri::AppHandle,
+) -> Result<(), String> {
+    let close_after_injected = match state.get_bool_option("misc_close_after_injected") {
+        Ok(value) => value,
+        Err(error) => {
+            dialogs::show_error("Latite Client", &error);
+            return Err(error);
+        }
+    };
+
     let result = launcher::inject(state.inner(), request).await;
 
     if let Err(error) = &result {
         dialogs::show_error("Latite Client", error);
+    } else if close_after_injected {
+        app_handle.exit(0);
     }
 
     result
